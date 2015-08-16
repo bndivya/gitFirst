e9ca5d9b31039570cfad273d9e0f68650285c321
Divya B N <bndivya13@gmail.com>
2015-08-16 19:36:57 +0530

    Divya | First commit

---
+++ Spring_first/src/First.java
@@ -0,0 +1,12 @@
+
+public class First {
+	 private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+}
